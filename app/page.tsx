a2ad8b9a3f1b2f9bf21ac24a0570baf6914b28de
jhay <79243795+jjhay-bot@users.noreply.github.com>
2024-08-02 21:28:16 +0800

    added basic routes
    - added basic routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,44 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import styles from '@/app/ui/home.module.css';
+import { lusitana } from './ui/fonts';
+import Image from 'next/image';
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+
+      <div className={styles.shape} />
+
+      <div className="flex flex-col h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         {/* <AcmeLogo /> */}
+
+        {/* SAMPLE - OVERFLOW, WIDTH, HEIGHT, FULL, FIT */}
+        {/* <div className='h-2/3 red w-full flex'>
+          <div>1jkjjkjkj</div>
+          <div className='pink max-w-[100px] overflow-auto'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero ad, modi earum placeat magni fugiat quos maiores natus impedit sed ea delectus quidem facere sequi est dolor? Maiores, at nulla.
+          </div>
+        </div>
+
+        <div className='h-1/3 red flex justify-end self-start'>
+          <div className='max-w-[100px] max-h-[100px] overflow-x-auto overflow-y-hidden text-left'>
+            <div className='pink text-nowrap h-full w-fit'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero ad, modi earum placeat magni fugiat quos maiores natus impedit sed ea delectus quidem facere sequi est dolor? Maiores, at nulla.
+            </div>
+          </div>
+        </div> */}
       </div>
+
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <div
+            className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
+          />
+          <p
+            className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+          >
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
@@ -26,6 +54,20 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshot of the dashboard project showing mobile version"
+          />
         </div>
       </div>
     </main>
