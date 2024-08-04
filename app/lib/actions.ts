44bdc7f77bbab014054b39a3c2062adbd698dffd
jhay <79243795+jjhay-bot@users.noreply.github.com>
2024-08-05 00:25:02 +0800

    Update actions.ts

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -101,6 +101,18 @@ export async function updateInvoice(
   redirect('/dashboard/invoices');
 }
 
+export async function deleteInvoice(id: string) {
+  // throw new Error("Failed to Delete Invoice");
+
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath("/dashboard/invoices");
+    return { message: "Deleted Invoice." };
+  } catch (error) {
+    return { message: "Database Error: Failed to Delete Invoice." };
+  }
+}
+
 export async function authenticate(
   prevState: string | undefined,
   formData: FormData,
